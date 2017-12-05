d997a07e98fdff0e266e6034c5083fec631b3d1f
Andrew Morgan <33350442+nagroma@users.noreply.github.com>
2017-12-04 19:56:37 -0600

    Update PostDeploy.ps1

--- azure-rm/scripts/PostDeploy.ps1
+++ azure-rm/scripts/PostDeploy.ps1
@@ -1,49 +1,14 @@
 Param(
     [string]$ChocoPackages,
-    [bool]$PartsUnlimited,
-    [bool]$Extras,
     [string]$VmAdminUserName,
     [Security.SecureString]$VmAdminPassword
 )
 
 cls
-function buildVS 
-{
-    param
-    (
-        [parameter(Mandatory=$true)]
-        [String] $path,
-
-        [parameter(Mandatory=$false)]
-        [bool] $nuget = $true,
-        
-        [parameter(Mandatory=$false)]
-        [bool] $clean = $true
-    )
-    process
-    {
-        $msBuildExe = 'C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\msbuild.exe'
-
-        if ($nuget) {
-            Write-Host "Restoring NuGet packages" -foregroundcolor green
-            nuget restore "$($path)"
-        }
-
-        if ($clean) {
-            Write-Host "Cleaning $($path)" -foregroundcolor green
-            & "$($msBuildExe)" "$($path)" /t:Clean /m
-        }
-
-        Write-Host "Building $($path)" -foregroundcolor green
-        & "$($msBuildExe)" "$($path)" /t:Build /m
-    }
-}
 
 New-Item "c:\choco" -type Directory -force | Out-Null
-$LogFile = "c:\choco\Script.log"
+$LogFile = "c:\choco\PostDeploy.log"
 $ChocoPackages | Out-File $LogFile -Append
-$PartsUnlimited | Out-File $LogFile -Append
-$Extras | Out-File $LogFile -Append
 $VmAdminUserName | Out-File $LogFile -Append
 $VmAdminPassword | Out-File $LogFile -Append
 
@@ -69,48 +34,15 @@ if (-not [String]::IsNullOrWhiteSpace($ChocoPackages)){
     }
 }
 
-if ($PartsUnlimited){
-    "Configuring PartsUnlimitedHOL" | Out-File $LogFile -Append
-    Invoke-Command -ScriptBlock {
-        $slnPath = "$env:userprofile\Desktop\PartsUnlimitedHOL"
-        "slnPath: $slnPath"
-        npm install bower -g
-        npm install grunt-cli -g
-        Copy-Item C:\Python27\python.exe C:\Python27\python2.exe
-        New-Item $slnPath -type directory -force
-        git clone https://github.com/Microsoft/PartsUnlimited.git $slnPath # The error message "CategoryInfo : NotSpecified: (Switched to branch..." is a documented issues but no workaround.  This still works fine.
-        #Sometimes the \AppData\Roaming\npm gets added to the path and some times it doesn't. This makes sure.
-        "Adding $env:userprofile\AppData\Roaming\npm to path"
-        $AddedLocation ="$env:userprofile\AppData\Roaming\npm"
-        $Reg = "Registry::HKLM\System\CurrentControlSet\Control\Session Manager\Environment"
-        $OldPath = (Get-ItemProperty -Path "$Reg" -Name PATH).Path
-        "OldPath: $OldPath"
-        if(-Not $OldPath.Contains($AddedLocation)) {
-            $NewPath= $OldPath + ';' + $AddedLocation
-            Set-ItemProperty -Path "$Reg" -Name PATH -Value $NewPath
-            $UpdatedPath = (Get-ItemProperty -Path "$Reg" -Name PATH).Path
-            "UpdatedPath: $UpdatedPath"
-        }
+"Configuring Extras" | Out-File $LogFile -Append
+Invoke-Command -ScriptBlock {
+    # Show file extensions (have to restart Explorer for this to take effect if run maually - Stop-Process -processName: Explorer -force)
+    Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name HideFileExt -Value "0"
 
-        refreshenv
-        buildVS "$slnPath\PartsUnlimited.sln" -nuget $true -clean $false
-        Remove-Item $slnPath\src\PartsUnlimitedWebsite\node_modules -Force -Recurse
-        buildVS "$slnPath\PartsUnlimited.sln" -nuget $true -clean $true
-    } -ComputerName $env:COMPUTERNAME -Credential $credential | Out-File $LogFile -Append
-}
+    Set-TimeZone -Name "Eastern Standard Time"
 
-#A few more settings that I like but are not required for the PartsUnlimitedHOL
-if ($Extras){
-    "Configuring Extras" | Out-File $LogFile -Append
-    Invoke-Command -ScriptBlock {
-        # Show file extensions (have to restart Explorer for this to take effect if run maually - Stop-Process -processName: Explorer -force)
-        Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name HideFileExt -Value "0"
+    Enable-WindowsOptionalFeature –online –featurename IIS-WebServerRole
 
-        Set-TimeZone -Name "Eastern Standard Time"
-
-        Enable-WindowsOptionalFeature –online –featurename IIS-WebServerRole
-
-    } -ComputerName $env:COMPUTERNAME -Credential $credential | Out-File $LogFile -Append
-}
+} -ComputerName $env:COMPUTERNAME -Credential $credential | Out-File $LogFile -Append
 
 Disable-PSRemoting -Force
